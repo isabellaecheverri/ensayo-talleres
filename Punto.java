3a37af3f9390f15434af25fa3f2c0239d4fe84f6
isabellaecheverri <78049340+isabellaecheverri@users.noreply.github.com>
2021-01-27 08:35:16 -0500

    Update Punto.java

--- Punto.java
+++ Punto.java
@@ -10,7 +10,7 @@ package Taller1;
  * @author User
  */
 /**
- * La clase Punto tiene la intenciÛn de representar coordenadas en el espacio y calcular su distancia.
+ * La clase Punto tiene la intenci√≥n de representar coordenadas en el espacio y calcular su distancia.
  * 
 * @author Mauricio Toro, Andres Paez
  * @version 1
@@ -18,80 +18,80 @@ package Taller1;
  
 import java.lang.Math;
 
-public class Punto {????
+public class Punto {
  
     private double x, y;
  
     /**
      * Se inicializan los atributos de clase
      */
-    public Punto(double x, double y) {????
+    public Punto(double x, double y) {
         this.x = x;
         this.y = y;
-    }????
+    }
  
     /**
-     * MÈtodo para obtener la variable global x.
+     * M√©todo para obtener la variable global x.
      *
      * @return eje coordenado x
      */
-    public double x() {????
+    public double x() {
         return x;
-    }????
+    }
  
     /**
-     * MÈtodo para obtener la variable global y.
+     * M√©todo para obtener la variable global y.
      *
      * @return eje coordenado y
      */
-    public double y() {????
+    public double y() {
         return y;
-    }????
+    }
  
     /**
-     * MÈtodo para obtener el radio polar, en este caso se puede obtener por medio del teorema de pit·goras.
+     * M√©todo para obtener el radio polar, en este caso se puede obtener por medio del teorema de pit√°goras.
      * 
      *
      * @return radio polar
      *
-     * @see <a href="http://mathworld.wolfram.com/PolarCoordinates.html"> Ver m·s <a/>
+     * @see <a href="http://mathworld.wolfram.com/PolarCoordinates.html"> Ver m√°s <a/>
      */
 
-    public double radioPolar() {????
+    public double radioPolar() {
         return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
-    }????
+    }
  
 
     /**
-     * MÈtodo para obtener el ·ngulo polar, en este caso se puede obtener por medio de la tangente inversa.
+     * M√©todo para obtener el √°ngulo polar, en este caso se puede obtener por medio de la tangente inversa.
      * 
      *
      * @return angulo polar
      *
-     * @see <a href="http://mathworld.wolfram.com/PolarCoordinates.html"> Ver m·s <a/>
+     * @see <a href="http://mathworld.wolfram.com/PolarCoordinates.html"> Ver m√°s <a/>
      */
-    public double anguloPolar() {????
+    public double anguloPolar() {
           return Math.atan(y/x);
-    }????
+    }
  
     /**
-     * MÈtodo para obtener la distacia euclidiana. La distacia
-     * euclidiana o euclÌdea es la distancia "ordinaria" (que se medirÌa con una regla) entre 
-     * dos puntos de un espacio euclÌdeo, la cual se deduce a partir del teorema de Pit·goras.
+     * M√©todo para obtener la distacia euclidiana. La distacia
+     * euclidiana o eucl√≠dea es la distancia "ordinaria" (que se medir√≠a con una regla) entre 
+     * dos puntos de un espacio eucl√≠deo, la cual se deduce a partir del teorema de Pit√°goras.
      * En otras palabras es halla el radio polar a la diferencia entre los dos puntos.
      *
      *
      *
      * @return distancia euclidiana (radio polar a la diferencia de dos puntos)
      *
-     * @param otro Este par·metro hace referencia a otro Punto en el espacio con es cual se desea
+     * @param otro Este par√°metro hace referencia a otro Punto en el espacio con es cual se desea
      * comparar el Punto desde el cual fue llamado.
-     * @see <a href="http://mathworld.wolfram.com/Distance.html"> Ver m·s <a/>
+     * @see <a href="http://mathworld.wolfram.com/Distance.html"> Ver m√°s <a/>
      */
 
-    public double distanciaEuclidiana(Punto otro) {????
-    			double x2 = otro.x;
+    public double distanciaEuclidiana(Punto otro) {
+    	   double x2 = otro.x;
           double y2 = otro.y;
-					return Math.sqrt( Math.pow((x2-x),2)+Math.pow((y2-y),2));
-    }????
-}????
+	  return Math.sqrt( Math.pow((x2-x),2)+Math.pow((y2-y),2));
+    }
+}
